f349a069baec9f1e8b2562211f49e75b9745db40
unknown <tribhuvanbachhu@gmail.com>
2026-03-06 09:45:07 +0530

    first commit

---
+++ src/main/java/com/example/cserender/CserenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.cserender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CserenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CserenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cserender/CserenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cserender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CserenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
